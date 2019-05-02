231fad40a6da8b28b42bff70bef2ea4dc1d058db
mrmullarky <conormullarky@gmail.com>
2019-05-02 17:29:18 +0100

    Extra features added
    Added scroll to top button and transition
    fixed responsive issues

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function navResponse() {
     }
   }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+  if(document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    document.getElementById("toTopButton").style.opacity = 1;
+  } else {;
+    document.getElementById("toTopButton").style.opacity = 0;
+  }
+}
+
+function backToTop(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
